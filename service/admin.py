b003f518d19b529f1f108b14de6684427e415066
ayyoob150 <ayyoob150@gmail.com>
2024-03-26 14:58:36 +0530

    create super user

--- service/admin.py
+++ service/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from service.models import Product
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name' , 'price' , 'description')
+    
+admin.site.register(Product,ProductAdmin)
 
 # Register your models here.
